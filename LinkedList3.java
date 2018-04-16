d5be19531b387fa1957ebf8c0fdf3d5f35cb6fb7
rajendercheraku <38342614+rajendercheraku@users.noreply.github.com>
2018-04-16 20:47:52 +0530

    Update LinkedList3.java

--- LinkedList3.java
+++ LinkedList3.java
@@ -1,6 +1,4 @@
-// Java program to count number of nodes in a linked list
- 
-/* Linked list Node*/
+
 class Node
 {
     int data;
@@ -8,26 +6,23 @@ class Node
     Node(int d)  { data = d;  next = null; }
 }
  
-// Linked List class
+
 class LinkedList3
 {
-    Node head;  // head of list
- 
-    /* Inserts a new Node at front of the list. */
+    Node head;  
     public void push(int new_data)
     {
-        /* 1 & 2: Allocate the Node &
-                  Put in the data*/
+       
         Node new_node = new Node(new_data);
  
-        /* 3. Make next of new Node as head */
+      
         new_node.next = head;
  
-        /* 4. Move the head to point to new Node */
+       
         head = new_node;
     }
  
-    /* Returns count of nodes in linked list */
+    
     public int getCount()
     {
         Node temp = head;
@@ -40,12 +35,10 @@ class LinkedList3
         return count;
     }
  
-    /* Drier program to test above functions. Ideally
-       this function should be in a separate user class.
-       It is kept here to keep code compact */
+    
     public static void main(String[] args)
     {
-        /* Start with the empty list */
+       
         LinkedList3 llist = new LinkedList3();
         llist.push(1);
         llist.push(3);
