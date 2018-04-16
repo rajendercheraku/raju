6229c9af1ca351db77bc7a6f4e502def2e2abe27
rajendercheraku <38342614+rajendercheraku@users.noreply.github.com>
2018-04-16 20:26:24 +0530

    Update LinkedList1.java

--- LinkedList1.java
+++ LinkedList1.java
@@ -1,8 +1,8 @@
 class LinkedList1
 {
-    Node head; // head of list
+    Node head;
  
-    /* Linked list Node*/
+    
     class Node
     {
         int data;
@@ -14,35 +14,35 @@ class LinkedList1
         }
     }
  
-    /* Given a key, deletes the first occurrence of key in linked list */
+   
     void deleteNode(int key)
     {
-        // Store head node
+       
         Node temp = head, prev = null;
  
-        // If head node itself holds the key to be deleted
+
         if (temp != null && temp.data == key)
         {
             head = temp.next; // Changed head
             return;
         }
  
-        // Search for the key to be deleted, keep track of the
-        // previous node as we need to change temp.next
+       
+   
         while (temp != null && temp.data != key)
         {
             prev = temp;
             temp = temp.next;
         }    
  
-        // If key was not present in linked list
+       
         if (temp == null) return;
  
-        // Unlink the node from linked list
+        
         prev.next = temp.next;
     }
  
-    /* Inserts a new Node at front of the list. */
+   
     public void push(int new_data)
     {
         Node new_node = new Node(new_data);
@@ -50,8 +50,8 @@ class LinkedList1
         head = new_node;
     }
  
-    /* This function prints contents of linked list starting from
-        the given node */
+
+       
     public void printList()
     {
         Node tnode = head;
@@ -62,9 +62,7 @@ class LinkedList1
         }
     }
  
-    /* Drier program to test above functions. Ideally this function
-    should be in a separate user class. It is kept here to keep
-    code compact */
+   
     public static void main(String[] args)
     {
         LinkedList1 llist = new LinkedList1();
@@ -77,7 +75,7 @@ class LinkedList1
         System.out.println("\nCreated Linked list is:");
         llist.printList();
  
-        llist.deleteNode(1); // Delete node at position 4
+        llist.deleteNode(1);
  
         System.out.println("\nLinked List after Deletion at position 4:");
         llist.printList();
